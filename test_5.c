a392e86a19fb163c7c970cb72e635e76782bb9e0
bchui <13244575+bchui114514@user.noreply.gitee.com>
2023-08-04 12:00:31 +0000

    update test_5.c.
    Signed-off-by: bchui <13244575+bchui114514@user.noreply.gitee.com>

--- test_5.c
+++ test_5.c
@@ -83,3 +83,126 @@ int main()
 	printf("%d\n", my_strlen(arr));
 	return 0;
 }
+
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c, c++, ++a, a++;
+//	b += a++ + c;
+//	printf("a = %d,b = %d,c = %d\n", a, b, c);
+//	return 0;
+//}
+
+////输出给定数字二进制中1的个数
+//int function(int n)
+//{
+//	int count = 0;
+//	////while (n)
+//	////{
+//	////	if (n % 2 == 1)
+//	////		count++;
+//	////	n /= 2;
+//	////}
+//	//int i = 0;
+//	//for ( i = 0; i < 32; i++)
+//	//{
+//	//	if (((n >> i) & 1) == 1)
+//	//		count++;
+//	//}
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//
+//	return count;
+//}
+//
+//int main()
+//{
+//	int n = -1;
+//	printf("有%d个1。", function(n));
+//	return 0;
+//}
+
+//判断一个数是否为2的n次方
+//void function(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	if (count == 1)
+//	{
+//		printf("是");
+//	}
+//	else
+//	{
+//		printf("否");
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int num = 16;
+//	function(num);
+//	return 0;
+//}
+
+//输出两个数二进制位不同的个数
+//int main()
+//{
+//	int m, n;
+//	scanf("%d %d", &m, &n);
+//	int count = 0;
+//	//for (int i = 0; i < 32; i++)
+//	//{
+//	//	if (((m >> i) & 1) != ((n >> i) & 1))
+//	//		count++;
+//	//}
+//	int ret = m ^ n;//异或 相同为0相异为1
+//	while (ret)
+//	{
+//		ret = ret & (ret - 1);
+//		count++;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//获取一个整数二进制序列中所有的偶数位和奇数位，并分别打印
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d", ((n >> i) & 1));
+//	}
+//	printf("\n");
+//	for (int i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d", ((n >> i) & 1));
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+//交换两个整型变量（不创建临时变量）
+int main()
+{
+	int m, n;
+	scanf("%d %d", &m, &n);
+	printf("%d %d\n", m, n);
+	m = m ^ n;
+	n = m ^ n;
+	m = m ^ n;
+	printf("%d %d\n", m, n);
+
+	return 0;
+}
